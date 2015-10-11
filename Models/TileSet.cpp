4a07c9dab441c7573c43ae623eec204d66ff92f4
Nicolas <nicolasfernandezlema@gmail.com>
2015-10-11 12:54:11 -0300

    Revert "agrego algoritmo de camino minimo"
    This reverts commit 329f5d145bcbe476785e1005e9d1a63a2124c81c.

--- Models/TileSet.cpp
+++ Models/TileSet.cpp
@@ -7,6 +7,8 @@
 
 #include "TileSet.h"
 
+using namespace std;
+
 TileSet::TileSet(int ancho, int alto) {
 	this -> ancho = ancho;
 	this -> alto = alto;
@@ -62,83 +64,4 @@ bool TileSet::sectorEstaBloqueado(SDL_Point origen, SDL_Point fin){
 	}
 
 	return false;
-}
-
-std::list<SDL_Point> TileSet::obtenerCamino(SDL_Point origen, SDL_Point destino){
-	std::list <SDL_Point> camino;
-	camino.push_front(origen);
-	camino.push_front(destino);
-	return camino;
-}
-
-//Devuelve todas las posiciones adyacentes a la baldosa, por las que se puede caminar
-std::list<SDL_Point> TileSet::vecinos(SDL_Point baldosa){
-	list<SDL_Point>lista_de_vecinos;
-	int x = baldosa.x;
-	int y = baldosa.y;
-	for (int i=-1; i<2; i++){
-		for (int j=-1; j<2; j++){
-			int x_v = x+i;
-			int y_v = y+j;
-			if (this->esVecino({x_v,y_v},baldosa)){
-					lista_de_vecinos.push_front({x_v,y_v});
-			}
-		}
-	}
-	return lista_de_vecinos;
-}
-
-
-//Devuelve el valor heuristico de ir de "a" a "b"
-int TileSet::distancia(SDL_Point a, SDL_Point b){
-	return sqrt( pow((b.x - a.x),2) + pow((b.y - a.y), 2));
-}
-
-//Devuelve el valor de ir de "a" a "b" en la grilla (a y b son vecinos)
-//Devuelve -1 si la arista no existe
-int TileSet::valorArista(SDL_Point a, SDL_Point b){
-	if  (!(this->esVecino(a,b))){
-		return -1;
-	}
-	return this->distancia(a,b);
-}
-
-bool TileSet::esVecino(SDL_Point a, SDL_Point b){
-	if (a.x >= 0 and a.y >= 0 and a.x < this->ancho and a.y < this->alto){
-		if (b.x >= 0 and b.y >= 0 and b.x < this->ancho and b.y < this->alto){
-			if (this->distancia(a,b) >= 1 and (this->distancia(a,b)) < 2){
-				if (!(this->matriz[a.x][a.y])or(this->matriz[b.x][b.y])){
-					return true;
-				}
-			}
-		}
-	}
-	return false;
-}
-
-pointMap TileSet::caminoMinimo(SDL_Point origen, SDL_Point destino){
-	PriorityQueue<SDL_Point> frontera;
-	frontera.put(origen, 0);
-	pointMap desde_donde_vino;
-	costMap costo_hasta_ahora;
-	desde_donde_vino [origen] = origen;
-	costo_hasta_ahora[origen] = 0;
-
-	while (!frontera.empty()) {
-		auto actual = frontera.get();
-		if (actual.x == destino.x and actual.y == destino.y) {
-			return desde_donde_vino;
-		}
-
-		for (auto prox : this->vecinos(actual)) {
-			int nuevo_costo = costo_hasta_ahora[actual] + this->valorArista(actual, prox);
-			if (!costo_hasta_ahora.count(prox) || nuevo_costo < costo_hasta_ahora[prox]) {
-				costo_hasta_ahora[prox] = nuevo_costo;
-				int prioridad = nuevo_costo + this->distancia(prox, destino); // distancia es la heuristica
-				frontera.put(prox, prioridad);
-				desde_donde_vino[prox] = actual;
-			}
-		}
-	}
-	return desde_donde_vino; // si sale del while es porque no llego al destino, el camino es hasta el lugar mas cercano al destino.
 }
