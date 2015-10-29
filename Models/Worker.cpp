1a85a99be4e3568f27590e934dc6f900603c9943
Dario Miñones <dminones@gmail.com>
2015-10-29 14:23:58 -0300

    Agrego Worker con este completamos todos los modelos basicos

---
+++ Models/Worker.cpp
@@ -0,0 +1,18 @@
+/*
+ * Worker.cpp
+ *
+ *  Created on: Oct 29, 2015
+ *      Author: dario
+ */
+
+#include "Worker.h"
+
+Worker::Worker() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Worker::~Worker() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ Models/Worker.h
@@ -0,0 +1,19 @@
+/*
+ * Worker.h
+ *
+ *  Created on: Oct 29, 2015
+ *      Author: dario
+ */
+
+#ifndef MODELS_WORKER_H_
+#define MODELS_WORKER_H_
+
+#include "MobileModel.h"
+
+class Worker: public MobileModel {
+public:
+	Worker();
+	virtual ~Worker();
+};
+
+#endif /* MODELS_WORKER_H_ */
