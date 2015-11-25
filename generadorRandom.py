32468a9554fe65ac0498fadcb1df5f17f092fe8b
FedeLonghi <fedee.29@hotmail.com>
2015-11-24 21:43:25 -0300

    -

--- generadorRandom.py
+++ generadorRandom.py
@@ -1,1 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#  main.py
+#  
+#  Copyright 2015 FedeLonghi <fedelonghi@fedelonghi-dell>
+#  
+#  This program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2 of the License, or
+#  (at your option) any later version.
+#  
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  
+#  You should have received a copy of the GNU General Public License
+#  along with this program; if not, write to the Free Software
+#  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+#  MA 02110-1301, USA.
+#  
+#  
+import random
+
+def generarMapa (ancho, alto, nJugadores):
+	
+	
+	
+
+def main():
+	NOMBRE_MAPA = ""
+	archivo = open(NOMBRE_MAPA)
+	
+	
+	return 0
+
+if __name__ == '__main__':
+	main()
 
